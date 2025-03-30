6dc713f9c69f601dc04c49b463273a66b5dd68d5
Himanshu <himanshu.jadoun@beyowned.com>
2025-03-30 22:08:58 +0530

    Debugging DB connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+console.log("DB CONFIG:", {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ? "******" : "Not Set",
+    database: process.env.DB_NAME
+});
+
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -10,10 +17,11 @@ const db = mysql.createConnection({
 
 db.connect(err => {
     if (err) {
-        console.error('Database connection failed:', err);
+        console.error('❌ Database connection failed:', err);
         process.exit(1);
+    } else {
+        console.log('✅ Connected to MySQL');
     }
-    console.log('✅ Connected to MySQL');
 });
 
 module.exports = db;
